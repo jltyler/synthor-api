12c8d7a0b89bacdd7cbaed5d2d690e4d45d16902
jltyler <john.lucas.tyler@gmail.com>
2017-08-30 14:11:33 -0400

    Add second oscillator
    - Compressed lots of the properties into single lines

--- app/models/patch.js
+++ app/models/patch.js
@@ -7,91 +7,50 @@ const patchSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  osc1Volume: {
-    type: Number,
-    required: true
-  },
-  osc1Octave: {
-    type: Number,
-    required: true
-  },
-  osc1Detune: {
-    type: Number,
-    required: true
-  },
+  // Oscillator 1 settings
   osc1Waveform: {
     type: String,
     required: true,
     enum: ['sawtooth', 'sine', 'square', 'triangle']
   },
-  osc1Unison: {
-    type: Number,
-    required: true
+  osc1Volume: { type: Number, required: true },
+  osc1Octave: { type: Number, required: true },
+  osc1Detune: { type: Number, required: true },
+  osc1Unison: { type: Number, required: true },
+  osc1Panning: { type: Number, required: true },
+  osc1TremoloAmp: { type: Number, required: true },
+  osc1TremoloFreq: { type: Number, required: true },
+  osc1Attack: { type: Number, required: true },
+  osc1Decay: { type: Number, required: true },
+  osc1Sustain: { type: Number, required: true },
+  osc1Release: { type: Number, required: true },
+  // Oscillator 2 settings
+  osc2Waveform: {
+    type: String,
+    required: true,
+    enum: ['sawtooth', 'sine', 'square', 'triangle']
   },
-  osc1Panning: {
-    type: Number,
-    required: true
-  },
-  osc1TremoloAmp: {
-    type: Number,
-    required: true
-  },
-  osc1TremoloFreq: {
-    type: Number,
-    required: true
-  },
-  osc1Attack: {
-    type: Number,
-    required: true
-  },
-  osc1Decay: {
-    type: Number,
-    required: true
-  },
-  osc1Sustain: {
-    type: Number,
-    required: true
-  },
-  osc1Release: {
-    type: Number,
-    required: true
-  },
-  filterFrequency: {
-    type: Number,
-    required: true
-  },
-  filterQ: {
-    type: Number,
-    required: true
-  },
-  filterEnv: {
-    type: Number,
-    required: true
-  },
-  filterTremoloAmp: {
-    type: Number,
-    required: true
-  },
-  filterTremoloFreq: {
-    type: Number,
-    required: true
-  },
-  filterAttack: {
-    type: Number,
-    required: true
-  },
-  filterDecay: {
-    type: Number,
-    required: true
-  },
-  filterSustain: {
-    type: Number,
-    required: true
-  },
-  filterRelease: {
-    type: Number,
-    required: true
-  },
+  osc2Volume: { type: Number, required: true },
+  osc2Octave: { type: Number, required: true },
+  osc2Detune: { type: Number, required: true },
+  osc2Unison: { type: Number, required: true },
+  osc2Panning: { type: Number, required: true },
+  osc2TremoloAmp: { type: Number, required: true },
+  osc2TremoloFreq: { type: Number, required: true },
+  osc2Attack: { type: Number, required: true },
+  osc2Decay: { type: Number, required: true },
+  osc2Sustain: { type: Number, required: true },
+  osc2Release: { type: Number, required: true },
+  // Filter settings
+  filterFrequency: { type: Number, required: true },
+  filterQ: { type: Number, required: true },
+  filterEnv: { type: Number, required: true },
+  filterTremoloAmp: { type: Number, required: true },
+  filterTremoloFreq: { type: Number, required: true },
+  filterAttack: { type: Number, required: true },
+  filterDecay: { type: Number, required: true },
+  filterSustain: { type: Number, required: true },
+  filterRelease: { type: Number, required: true },
   _owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
