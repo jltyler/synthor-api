b76d603d74a8c0c3e06b43eeed735a0c6254402b
jltyler <john.lucas.tyler@gmail.com>
2017-08-31 17:37:17 -0400

    Changed root to show name of API

--- app/controllers/root.js
+++ app/controllers/root.js
@@ -6,7 +6,7 @@ const controller = require('lib/wiring/controller')
 const root = (req, res) => {
   res.json({
     index: {
-      title: 'Express Template',
+      title: 'Synthor API',
       environment: req.app.get('env')
     }
   })
