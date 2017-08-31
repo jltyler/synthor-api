5fc7401905982028406893446af881a47c01f08d
jltyler <john.lucas.tyler@gmail.com>
2017-08-31 10:27:40 -0400

    Add waveform setting for oscillator tremolo

--- app/models/patch.js
+++ app/models/patch.js
@@ -11,6 +11,7 @@ const patchSchema = new mongoose.Schema({
     type: Boolean,
     default: false
   },
+
   // Oscillator 1 settings
   osc1Waveform: {
     type: String,
@@ -22,12 +23,18 @@ const patchSchema = new mongoose.Schema({
   osc1Detune: { type: Number, required: true },
   osc1Unison: { type: Number, required: true },
   osc1Panning: { type: Number, required: true },
+  osc1TremoloWaveform: {
+    type: String,
+    required: true,
+    enum: ['sawtooth', 'sine', 'square', 'triangle']
+  },
   osc1TremoloAmp: { type: Number, required: true },
   osc1TremoloFreq: { type: Number, required: true },
   osc1Attack: { type: Number, required: true },
   osc1Decay: { type: Number, required: true },
   osc1Sustain: { type: Number, required: true },
   osc1Release: { type: Number, required: true },
+
   // Oscillator 2 settings
   osc2Waveform: {
     type: String,
@@ -41,10 +48,16 @@ const patchSchema = new mongoose.Schema({
   osc2Panning: { type: Number, required: true },
   osc2TremoloAmp: { type: Number, required: true },
   osc2TremoloFreq: { type: Number, required: true },
+  osc2TremoloWaveform: {
+    type: String,
+    required: true,
+    enum: ['sawtooth', 'sine', 'square', 'triangle']
+  },
   osc2Attack: { type: Number, required: true },
   osc2Decay: { type: Number, required: true },
   osc2Sustain: { type: Number, required: true },
   osc2Release: { type: Number, required: true },
+
   // Filter settings
   filterFrequency: { type: Number, required: true },
   filterQ: { type: Number, required: true },
